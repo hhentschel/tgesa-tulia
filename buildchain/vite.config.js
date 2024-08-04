4da48da2eae9692a3ad21b348ea0a451000b99ce
Henrik Hentschel <henrik.hentschel@zeix.com>
2024-08-04 18:03:33 +0200

    change criticalcss path

--- buildchain/vite.config.js
+++ buildchain/vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig(({command}) => ({
   plugins: [
     critical({
       criticalUrl: 'https://google.com',
-      criticalBase: '../dist/criticalcss/',
+      criticalBase: './cms/web/dist/criticalcss/',
       criticalPages: [
         {
           uri: '/',
