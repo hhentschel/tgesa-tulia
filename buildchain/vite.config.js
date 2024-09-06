4a2eef8ed8489cd4ababe3778ed47aa6359d76c6
Henrik Hentschel <henrik.hentschel@zeix.com>
2024-09-06 12:33:15 +0200

    change vite config output dir

--- buildchain/vite.config.js
+++ buildchain/vite.config.js
@@ -14,7 +14,7 @@ import * as path from 'path'
 export default defineConfig(({command}) => ({
   base: command === 'serve'? '' : '/dist/',
   build: {
-    outDir: '../web/dist',
+    outDir: '../cms/web/dist',
     emptyOutDir: true,
     manifest: true,
     rollupOptions: {
